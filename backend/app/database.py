e345e961b3587717e43f07aa83940709078643d6
DrEditxon <133454118+DrEditxon@users.noreply.github.com>
2026-04-26 17:25:38 -0500

    Cambiando bd a SQLite

--- backend/app/database.py
+++ backend/app/database.py
@@ -3,11 +3,17 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from app.config import settings
 
+connect_args = {"check_same_thread": False} if settings.DATABASE_URL.startswith("sqlite") else {}
+engine_args = {} if settings.DATABASE_URL.startswith("sqlite") else {
+    "pool_pre_ping": True,
+    "pool_size": 10,
+    "max_overflow": 20,
+}
+
 engine = create_engine(
     settings.DATABASE_URL,
-    pool_pre_ping=True,
-    pool_size=10,
-    max_overflow=20,
+    connect_args=connect_args,
+    **engine_args
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
